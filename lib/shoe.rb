582a09103d1ebceb33f5fdf1e9dce94f7cb489a9
Khris Punzalan <khrispunzalan@gmail.com>
2019-06-17 04:01:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,19 +7,11 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-  
-  # def brand
-  #   @brand
-  # end
 
   def size=(size)
     @size = size
   end
   
-  # def size
-  #   @size
-  # end
-
   def cobble
     if self.condition = "new"
       puts "Your shoe is as good as new!"
