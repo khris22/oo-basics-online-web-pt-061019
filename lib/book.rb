0528eb0b3403010d9e39736698c2df405aa1f89e
Khris Punzalan <khrispunzalan@gmail.com>
2019-06-17 03:30:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,8 +7,9 @@ class Book
     end
 
     def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+      puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
 # class Book
